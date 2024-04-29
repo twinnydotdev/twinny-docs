9b9db781351eb11746383972b7e9bbb5edd1889e
rjmacarthy <richardmacarthy@hotmail.com>
2024-04-29 20:30:51 +0100

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,26 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+import tailwind from '@astrojs/tailwind';
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'https://github.com/rjmacarthy/twinny-docs.github.io',
+	integrations: [
+		starlight({
+			title: 'twinny',
+			social: {
+				github: 'https://github.com/rjmacarthy/twinny',
+			},
+			sidebar: [
+				{
+					label: 'Guides',
+					items: [
+						{ label: 'Get started', link: '/guides/get-started/' },
+					],
+				},
+			],
+			customCss: ['./src/tailwind.css'],
+		}),
+		tailwind({ applyBaseStyles: false }),
+	],
+});
