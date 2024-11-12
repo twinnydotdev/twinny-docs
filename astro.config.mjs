743dfce8899546e0b90b07bcd01ab1c996da9dbd
fishshi <2855691008@qq.com>
2024-11-12 14:44:05 +0800

    Add Chinese index

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,27 +4,65 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://github.com/rjmacarthy/twinny-docs.github.io',
+	site: 'https://twinnydotdev.github.io',
 	base: '/twinny-docs',
 	integrations: [
 		starlight({
 			title: 'twinny',
+			defaultLocale: 'root',
+			locales: {
+				root: { label: 'English', lang: 'en' },
+				'zh-cn': { label: '简体中文', lang: 'zh-CN' },
+			},
 			social: {
-				github: 'https://github.com/rjmacarthy/twinny',
+				github: 'https://github.com/twinnydotdev/twinny',
 				'x.com': 'https://x.com/twinnydotdev',
 			},
 			sidebar: [
 				{
 					label: '/',
 					items: [
-						{ label: 'Quick start', link: '/general/quick-start' },
-						{ label: 'Inference providers', link: '/general/providers' },
-						{ label: 'Supported models', link: '/general/supported-models' },
-						{ label: 'Chat', link: '/general/chat' },
-						{ label: 'Fill in middle', link: '/general/fill-in-middle' },
-						{ label: 'Keyboard shortcuts', link: '/general/keyboard-shortcuts' },
-						{ label: 'Symmetry Network', link: '/general/symmetry' },
-						{ label: 'Support twinny', link: '/general/support-twinny' },
+						{
+							label: 'Quick start', link: '/general/quick-start',
+							translations: {
+								'zh-CN': '快速开始'
+							}
+						},
+						{ label: 'Inference providers', link: '/general/providers',
+							translations: {
+								'zh-CN': '推理提供者'
+							}
+						},
+						{ label: 'Supported models', link: '/general/supported-models',
+							translations: {
+								'zh-CN': '支持的模型'
+							}
+						},
+						{ label: 'Chat', link: '/general/chat',
+							translations: {
+								'zh-CN': '对话'
+							}
+						},
+						{ label: 'Fill in middle', link: '/general/fill-in-middle',
+							translations: {
+								'zh-CN': '自动补全'
+							}
+						},
+						{ label: 'Keyboard shortcuts', link: '/general/keyboard-shortcuts',
+							translations: {
+								'zh-CN': '键盘快捷键'
+							}
+						},
+						{ label: 'Symmetry Network', link: '/general/symmetry',
+							translations: {
+								'zh-CN': 'Symmetry网络'
+							}
+						},
+						{ label: 'Support twinny', link: '/general/support-twinny',
+							translations: {
+								'zh-CN': '支持twinny'
+							}
+						},
 					],
 				},
 			],
